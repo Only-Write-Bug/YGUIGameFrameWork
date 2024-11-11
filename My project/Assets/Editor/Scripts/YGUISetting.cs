b7bdd4c59e5ff1569db0c14b43d3bad44e58e59b
KKKmoji <qwer123456qin@163.com>
2024-11-11 11:52:45 +0800

    [feature] Start setting up the YGUI editor settings window
    开始搭建YGUI的编辑器设置窗口

--- My project/Assets/Editor/Scripts/YGUISetting.cs
+++ My project/Assets/Editor/Scripts/YGUISetting.cs
@@ -1,8 +1,19 @@
 using System;
+using System.Collections.Generic;
 using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Editor.Scripts
 {
+    public class YGUISettingData
+    {
+        public Dictionary<string, string> labels = new Dictionary<string, string>();
+        public Dictionary<string, string> texts = new Dictionary<string, string>();
+        public Dictionary<string, bool> toggles = new Dictionary<string, bool>();
+        public Dictionary<string, Enum> dropdowns = new Dictionary<string, Enum>();
+    }
+    
     public class YGUISetting : EditorWindow
     {
         [MenuItem("YGUI/Settings")]
@@ -14,6 +25,23 @@ namespace Editor.Scripts
         private void CreateGUI()
         {
             var root = rootVisualElement;
+            root.Add(InitSaveBtn());
+        }
+
+        private Button InitSaveBtn()
+        {
+            return new Button(() =>
+            {
+                Debug.Log("Save Settings");
+            })
+            {
+                text = "Save",
+                style =
+                {
+                    width = Length.Percent(30),
+                    height = 25
+                }
+            };
         }
     }
 }
