c7ecce7cd2a586e7931b4d6e8005ae25958b3fb2
KKKmoji <qwer123456qin@163.com>
2024-11-18 14:46:48 +0800

    [feature] Added resource path settings in YGUISettings
    增加YGUISettings中资源路径设置

--- My project/Assets/Editor/Scripts/YGUISetting.cs
+++ My project/Assets/Editor/Scripts/YGUISetting.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using common.CustomDataStruct;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -9,15 +10,18 @@ namespace Editor.Scripts
 {
     public class YGUISettingData
     {
-        public Dictionary<string, string> labels = new Dictionary<string, string>();
-        public Dictionary<string, string> texts = new Dictionary<string, string>();
-        public Dictionary<string, bool> toggles = new Dictionary<string, bool>();
-        public Dictionary<string, Enum> dropdowns = new Dictionary<string, Enum>();
+        
     }
     
     public class YGUISetting : EditorWindow
     {
+        private const int DEFAULT_TITTLE_FONT_SIZE = 20;
+        private const int DEFAULT_ITEM_FONT_SIZE = 15;
+
+        private const string SELECT_ASSET_FOLDER_PATH = "select_asset_folder_path";
+        
         private YGUISettingData _data = new YGUISettingData();
+        
         [MenuItem("YGUI/Settings")]
         public static void OpenSettings()
         {
@@ -27,7 +31,35 @@ namespace Editor.Scripts
         private void CreateGUI()
         {
             var root = rootVisualElement;
+            root.style.flexDirection = FlexDirection.Column;
+            root.style.alignItems = Align.Center;
+            
+            root.Add(InitAssetBundleSettings());
             root.Add(InitSaveBtn());
+        }
+
+        private VisualElement InitAssetBundleSettings()
+        {
+            var container = CreateBox();
+            
+            container.Add(CreateTittle("AssetBundle Settings"));
+
+            var assetsPathContainer = CreateContainer();
+            assetsPathContainer.Add(CreateItemName("Assets Path:"));
+            var label = new Label
+            {
+                style = { fontSize = DEFAULT_ITEM_FONT_SIZE, }
+            };
+            assetsPathContainer.Add(label);
+            assetsPathContainer.Add(CreateItemButton("Modify", () =>
+            {
+                label.text = EditorUtility.OpenFolderPanel("select asset folder", "", "");
+            }));
+            container.Add(assetsPathContainer);
+            
+            container.Add(CreateUnderLine());
+            
+            return container;
         }
 
         private Button InitSaveBtn()
@@ -38,12 +70,94 @@ namespace Editor.Scripts
                 LocalSettingsUtil.SaveSettings(_data, ESettingsFilePath.YGUI);
             })
             {
-                text = "Save",
+                text = "Save Settings",
+                style =
+                {
+                    width = Length.Percent(90),
+                    height = 25,
+                    paddingTop = 5,
+                    paddingBottom = 5,
+                }
+            };
+        }
+
+        private VisualElement CreateBox()
+        {
+            return new VisualElement
+            {
+                style =
+                {
+                    flexDirection = FlexDirection.Column,
+                    alignItems = Align.Center,
+                    width = Length.Percent(90)
+                }
+            };
+        }
+
+        private VisualElement CreateContainer()
+        {
+            return new VisualElement
+            {
                 style =
                 {
                     flexDirection = FlexDirection.Row,
-                    width = Length.Percent(30),
-                    height = 25
+                    alignItems = Align.Center
+                }
+            };
+        }
+
+        private Label CreateTittle(string content)
+        {
+            return new Label
+            {
+                text = content,
+                style =
+                {
+                    fontSize = DEFAULT_TITTLE_FONT_SIZE,
+                    color = Color.white
+                }
+            };
+        }
+
+        private Label CreateItemName(string name)
+        {
+            return new Label
+            {
+                text = name,
+                style =
+                {
+                    fontSize = DEFAULT_ITEM_FONT_SIZE,
+                    backgroundColor = Color.white,
+                    color = Color.black
+                }
+            };
+        }
+
+        private Button CreateItemButton(string content, Action callback)
+        {
+            return new Button(() => callback?.Invoke())
+            {
+                text = content,
+                style =
+                {
+                    width = 50,
+                    height = 20,
+                }
+            };
+        }
+
+        private VisualElement CreateUnderLine()
+        {
+            return new VisualElement()
+            {
+                style =
+                {
+                    height = 2,
+                    backgroundColor = Color.white,
+                    marginTop = 3,
+                    marginBottom = 3,
+                    flexGrow = 1,
+                    width = Length.Percent(100)
                 }
             };
         }
