8e0972fed7959a43e93056337c554d0e4c607a1b
KKKmoji <qwer123456qin@163.com>
2024-11-19 18:18:30 +0800

    [feature] Introducing the Odin serializer
    引入Odin序列化器，为序列化Prefab做准备

---
+++ My project/Assets/Scripts/GameFrame/YAssetManage/PrefabsManage/PrefabSerializer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace GameFrame.YAssetManage.PrefabsManage
+{
+    public class PrefabSerializer
+    {
+        public static string Serialize(Object obj)
+        {
+            return "";
+        }
+
+        public static GameObject DeSerialize()
+        {
+            return null;
+        }
+    }
+}
