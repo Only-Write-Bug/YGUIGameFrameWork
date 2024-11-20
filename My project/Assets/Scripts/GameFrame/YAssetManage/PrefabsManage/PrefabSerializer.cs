266a7ebcffb9554ce066de233ff0d495ac93ff78
KKKmoji <qwer123456qin@163.com>
2024-11-20 18:51:04 +0800

    [feature] serialize prefab
    序列化预制体

--- My project/Assets/Scripts/GameFrame/YAssetManage/PrefabsManage/PrefabSerializer.cs
+++ My project/Assets/Scripts/GameFrame/YAssetManage/PrefabsManage/PrefabSerializer.cs
@@ -1,12 +1,55 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace GameFrame.YAssetManage.PrefabsManage
 {
+    [Serializable]
+    public class SerializablePrefab
+    {
+        public string prefabName;
+        public string prefabPath;
+        public List<SerializableGameObject> children = new List<SerializableGameObject>();
+    }
+
+    [Serializable]
+    public class SerializableGameObject
+    {
+        public string name;
+        public List<SerializableComponent> components = new List<SerializableComponent>();
+    }
+
+    [Serializable]
+    public class SerializableComponent
+    {
+        public string type;
+        public Dictionary<string, object> fields = new Dictionary<string, object>();
+    }
+    
     public class PrefabSerializer
     {
-        public static string Serialize(Object obj)
+        public static string Serialize(GameObject go)
         {
+            var serializablePrefab = new SerializablePrefab
+            {
+                prefabName = go.name,
+                children = serializeGameObjects(go)
+            };
+            
             return "";
+        }
+
+        private static List<SerializableGameObject> serializeGameObjects(GameObject go)
+        {
+            var result = new List<SerializableGameObject>();
+
+            foreach (var child in go.transform)
+            {
+                
+            }
+
+            return result;
         }
 
         public static GameObject DeSerialize()
